a95f70affd785ff106c829f421c55fc289e59b9a
m-ogore <ogoremarv@gmail.com>
2023-10-31 11:16:12 +0200

    Pep 8 style

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-#A program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)
-#loop through numbers 0-98 and print
-for i in range(0,99):
-    print("{} = {}".format(i,hex(i)))
+# A program that prints all numbers from 0 to 98 in decimal and hexadecimal
+
+# Loop through numbers 0-98 and print
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
