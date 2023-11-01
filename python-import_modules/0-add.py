91f36e1d9ae42b0d40021a2da96ec5c2a490d29d
m-ogore <ogoremarv@gmail.com>
2023-11-01 11:11:31 +0200

    added __name__

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,6 +6,11 @@
 
 add_0 = __import__('add_0')
 
-a = 1
-b = 2
-print("{} + {} = {}\n".format(a, b, add_0.add(a, b)))
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}\n".format(a, b, add_0.add(a, b)))
+
+if __name__ == "__main__":
+    main()
