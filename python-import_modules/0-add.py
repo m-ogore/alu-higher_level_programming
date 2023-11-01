db392e8fcb7c2bb5501638a701d7826463e41cbf
m-ogore <ogoremarv@gmail.com>
2023-11-01 11:13:00 +0200

    only 1 add_0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,13 +4,13 @@
 #from the file add_0.py 
 #and prints the result of the addition 1 + 2 = 3
 
-add_0 = __import__('add_0')
+s = __import__('add_0')
 
 
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}\n".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}\n".format(a, b, s.add(a, b)))
 
 if __name__ == "__main__":
     main()
