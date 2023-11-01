a6fb9b3bfa71afa149f35452550d7e10b3566b5c
m-ogore <ogoremarv@gmail.com>
2023-11-01 10:56:07 +0200

    importing modules

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#A program that imports the function add(a, b): 
+#from the file add_0.py 
+#and prints the result of the addition 1 + 2 = 3
+
+import add_0
+
+a = 1
+b = 2
+print("{} + {} = {}\n".format(a, b, add_0.add(a, b)))
