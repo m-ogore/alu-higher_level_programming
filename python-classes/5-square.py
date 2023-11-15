79c1c046d83cca4b0752541071d2737755473fe7
m-ogore <ogoremarv@gmail.com>
2023-11-15 08:18:56 +0200

    classes in python documented

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -13,13 +13,30 @@ Public instance method: def my_print(self): that prints in stdout the square wit
 if size is equal to 0, print an empty line
 You are not allowed to import any module
 '''
+
 class Square:
+            """
+        Initializes a new instance of the class.
+
+        Parameters:
+            size (int, optional): The size of the object. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+            
+            """
+        Get the value of the `size` property.
+
+        :return: The value of the `size` property.
+        """
     @property
     def size(self):
         return self.__size
